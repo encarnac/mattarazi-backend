af15bc316d7f19393d47f4b7a3a46277bb61eb7a
Colene Encarnado <encarnac@oregonstate.edu>
2024-03-26 17:10:24 -0700

    Added tabs and turned virtual field "Published" into ui field

--- src/collections/Products.js
+++ src/collections/Products.js
@@ -49,27 +49,136 @@ const Products = {
     delete: isAdminOrDev,
   },
   fields: [
+    // PRODUCT OVERVIEW GROUP
     {
-      name: "photo",
-      type: "upload",
-      relationTo: "photos",
-      required: false,
+      type: "tabs",
+      tabs: [
+        {
+          label: "Overview", // required
+          fields: [
+            {
+              name: "article",
+              type: "text",
+              required: true,
+              unique: true,
+              index: true,
+              hooks: {
+                beforeChange: [
+                  ({ value }) => {
+                    return value.toUpperCase();
+                  },
+                ],
+              },
+            },
+            {
+              name: "slideshow",
+              type: "array",
+              label: "Photo(s)",
+              maxRows: 3,
+              labels: {
+                singular: "Photo",
+                plural: "Photos",
+              },
+              fields: [
+                {
+                  name: "photo",
+                  type: "upload",
+                  relationTo: "photos",
+                  required: false,
+                  admin: {
+                    components: {
+                      Cell: PhotoCell,
+                    },
+                  },
+                },
+                {
+                  label: "caption",
+                  name: "alt",
+                  type: "text",
+                },
+              ],
+              admin: {
+                initCollapsed: false,
+                components: {
+                  RowLabel: ({ data, index }) => {
+                    return (
+                      data?.title || `Photo ${String(index).padStart(2, "0")}`
+                    );
+                  },
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    // PRODUCT DETAILS GROUP
+    {
+      type: "tabs",
+      tabs: [
+        {
+          label: "Product Details",
+          fields: [
+            {
+              name: "model",
+              type: "relationship",
+              relationTo: "models",
+              hasMany: true,
+              required: true,
+              index: true,
+            },
+            {
+              name: "material",
+              type: "relationship",
+              relationTo: "materials",
+              hasMany: true,
+              required: true,
+              index: true,
+            },
+            {
+              type: "row",
+              fields: [
+                {
+                  name: "color",
+                  type: "relationship",
+                  relationTo: "colors",
+                  hasMany: false,
+                  required: true,
+                  index: true,
+                },
+                {
+                  name: "pattern",
+                  type: "relationship",
+                  relationTo: "patterns",
+                  hasMany: false,
+                  required: true,
+                  index: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+
+    // SIDEBAR
+    {
+      name: "publishedOn",
+      type: "date",
       admin: {
-        components: {
-          Cell: PhotoCell,
+        position: "sidebar",
+        date: {
+          pickerAppearance: "dayAndTime",
         },
       },
-    },
-    {
-      name: "article",
-      type: "text",
-      required: true,
-      unique: true,
-      index: true,
       hooks: {
         beforeChange: [
-          ({ value }) => {
-            return value.toUpperCase();
+          ({ siblingData, value }) => {
+            if (siblingData._status === "published" && !value) {
+              return new Date();
+            }
+            return value;
           },
         ],
       },
@@ -81,49 +190,12 @@ const Products = {
       hasMany: false,
       required: true,
       index: true,
-    },
-    {
-      name: "model",
-      type: "relationship",
-      relationTo: "models",
-      hasMany: true,
-      required: true,
-      index: true,
-    },
-    {
-      type: "row",
-      fields: [
-        {
-          name: "color",
-          type: "relationship",
-          relationTo: "colors",
-          hasMany: false,
-          required: true,
-          index: true,
-        },
-        {
-          name: "pattern",
-          type: "relationship",
-          relationTo: "patterns",
-          hasMany: false,
-          required: true,
-          index: true,
-        },
-        {
-          name: "material",
-          type: "relationship",
-          relationTo: "materials",
-          hasMany: true,
-          required: true,
-          index: true,
-        },
-      ],
+      admin: { position: "sidebar" },
     },
     {
       name: "published",
-      type: "text",
+      type: "ui",
       admin: {
-        readOnly: true,
         components: {
           Cell: statusCell,
         },
